420ff6df7b9bdbf30d22a8662664dfa225debee3
hong-il <clroyan01@gmail.com>
2020-02-14 16:25:56 +0900

    Update security - video url

--- src/app/bypass-security.component.ts
+++ src/app/bypass-security.component.ts
@@ -8,9 +8,18 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browse
 export class BypassSecurityComponent {
   dangerousUrl: string;
   trustedUrl: SafeUrl;
+  dangerousVideoUrl: string;
+  videoUrl: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
     this.dangerousUrl = 'javascript:alert("github.com/hong-il")';
     this.trustedUrl = sanitizer.bypassSecurityTrustUrl(this.dangerousUrl);
+    this.updateVideoUrl('hong-il');
+  }
+
+  updateVideoUrl(id: string) {
+    this.dangerousVideoUrl = 'https://github.com/' + id;
+    this.videoUrl =
+        this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
   }
 }
